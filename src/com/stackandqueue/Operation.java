aee008228838ae85b0f3b6c54323e620296e9d33
bolishettianjali <bolishettianjali@gamil.com>
2021-11-16 12:37:37 +0530

    Added:Ability to create a stack

---
+++ src/com/stackandqueue/Operation.java
@@ -0,0 +1,14 @@
+package com.stackandqueue;
+
+public class Operation {
+    public static Stack addElement() {
+        Stack stacklist =new Stack();
+        stacklist.push(70);
+        stacklist.push(30);
+        stacklist.push(56);
+
+        stacklist.display();
+        return stacklist;
+
+    }
+}
